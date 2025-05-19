84b56ae82e0d07fc2e6edcd080b586c246d22969
Beatriz Batista <byah24@outlook.com>
2025-05-19 09:09:15 -0300

    Programa Finalizado

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// Console.WriteLine("---- Groot Chatbot ----");
+
+// string pergunta = "";
+
+// while (true)
+// {
+//     Console.Write("Pergunta:");
+//     pergunta = Console.ReadLine()!;
+//     if (pergunta == "Tchau")
+//     {
+//         Console.WriteLine("Eu sou Groot.");
+//         break;
+//     }
+
+//     Console.WriteLine("Eu sou Groot.");
+
+// }
+
+﻿Console.Clear();
+string pergunta = "";
+bool respostaEhTchau = false;
+Console.WriteLine("--- Groot Chatbot ---");
+while (!respostaEhTchau)
+{
+    Console.Write("Pergunta: ");
+    pergunta = Console.ReadLine()!;
+    respostaEhTchau = pergunta == "Tchau";
+    Console.WriteLine($"                          Resposta: Eu sou Groot{(respostaEhTchau ? "!" : ".")}");
+}
